1cfdc2bafc6beee004a51b23f547c89582463964
Jean Carlo.O <Jean Carlo.O.>
2024-02-23 13:45:37 -0500

    sin node modules

---
+++ js/libs/api/urls.js
@@ -0,0 +1,6 @@
+
+// se coloca una url base 
+export const URL_BASE = "http://localhost:4000"
+// se concatena la url base con companies y jobs
+export const URL_COMPANIES = `${URL_BASE}/companies`
+export const URL_JOBS= `${URL_BASE}/jobs`
